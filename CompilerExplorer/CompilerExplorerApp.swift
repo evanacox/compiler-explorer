20eeb487810f5c76956ed8ca600af675b24966ed
Evan Cox <evanacox00@gmail.com>
2022-06-21 10:41:18 -0400

    Initial Commit

---
+++ CompilerExplorer/CompilerExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CompilerExplorerApp.swift
+//  CompilerExplorer
+//
+//  Created by Evan Cox on 6/21/22.
+//
+
+import SwiftUI
+
+@main
+struct CompilerExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
